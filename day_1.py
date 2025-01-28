52981264c113cc6e554f09243496c1dd6fa5f779
Paul Tidings Steiner <adjeisteiner@gmail.com>
2025-01-28 10:01:12 +0000

    Day 1 completed

---
+++ day_1.py
@@ -0,0 +1,43 @@
+#1) Understanding Python's primitive data types:
+
+#a) Integers
+""" They're WHole numbers used to represent counts, indicies or discrete quantitie.
+They can be positive or negative """
+int('5') #5
+
+
+
+#b) Floats
+""" A float represents real nubers with decimal points. 
+They can also be used to represent fractions """
+float(5) #5.0
+
+#c) Strings
+"The boy"
+'3646 is a string'
+str(5) # '5'
+
+#d) Booleans
+True / False
+bool(0) #False
+bool(1) #True
+
+
+#2) assigning and manipulating variables
+var = 5
+this_var = 5
+this var = 5 #invalid
+1var = 5 #invalid
+var1 = 5
+Var1 = 5
+bool_var = True
+string_var = 'me'
+str_var = 'them'
+string_var + str_var = 'methem'
+string_var + ' ' + str_var = 'me them'
+
+#3) Type casting and conversions
+x = 10
+y = 2.5
+print(type(x))
+print(float(x)) #10.0
